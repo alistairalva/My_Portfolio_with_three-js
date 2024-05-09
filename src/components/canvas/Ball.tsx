1ee54c29f38b8eca384ab4a360473d46a2578e44
Alistair Alva <a_alva@fanshaweonline.ca>
2024-05-09 12:55:45 -0400

    Technology and Experience Section Done

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  useTexture,
+  // MeshProps,
+} from "@react-three/drei";
 
-const Ball = () => {
-  return <div>Ball</div>;
+import CanvasLoader from "../Loader";
+
+interface BallProps {
+  imgUrl: string;
+}
+
+const Ball: React.FC<BallProps> = ({ imgUrl }) => {
+  const [decal] = useTexture([imgUrl]);
+  // const meshRef = useRef<MeshProps>(null!);
+
+  // useFrame(() => {
+  //   if (meshRef.current) {
+  //     meshRef.current.rotation.y += 0.01;
+  //     meshRef.current.rotation.x += 0.01;
+  //   }
+  // });
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={1}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  );
 };
 
-export default Ball;
+interface BallCanvasProps {
+  icon: string;
+}
+
+const BallCanvas: React.FC<BallCanvasProps> = ({ icon }) => {
+  return (
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+    </Canvas>
+  );
+};
+export default BallCanvas;
