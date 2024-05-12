df5f77e5b57143b3689e6b5c41582939e295e5a5
Alistair Alva <a_alva@fanshaweonline.ca>
2024-05-12 16:08:02 -0400

    Form UI complete
    TODO: Complete the email.js functionality

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,106 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import email from "@emailjs/browser";
 
-const Contact = () => {
-  return <div>Contact</div>;
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../motion";
+
+const Contact: React.FC = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // try {
+    //   await email.send(
+    //     process.env.REACT_APP_EMAIL_SERVICE_ID!,
+    //     process.env.REACT_APP_EMAIL_TEMPLATE_ID!,
+    //     form,
+    //     process.env.REACT_APP_EMAIL_USER_ID!
+    //   );
+
+    //   setForm({ name: "", email: "", message: "" });
+    //   formRef.current?.reset();
+    //   alert("Message sent successfully!");
+    // } catch (error) {
+    //   alert("Failed to send message. Please try again later.");
+    // } finally {
+    //   setLoading(false);
+    // }
+  };
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 0.1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded 2xl"
+      >
+        <p className={styles.sectionSubText}>Get in Touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows={7}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none"
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+      <motion.div variants={slideIn("left", "tween", 0.2, 0.1)}></motion.div>
+    </div>
+  );
 };
 
-export default Contact;
+const ContactSection = SectionWrapper(Contact, "contact");
+export default ContactSection;
